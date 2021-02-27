ae4dfa9eaf3ff104a1a0f2d54629770044bccedc
Ruben Di Battista <rubendibattista@gmail.com>
2021-02-27 06:24:33 +0100

    Add debug file for macOS when running the App Bundle

--- run-desktop.py
+++ run-desktop.py
@@ -5,6 +5,28 @@ import os
 import sys
 import multiprocessing
 
+import logging
+from logging.handlers import RotatingFileHandler
+
+from jellyfin_mpv_shim.conffile import get as get_config_resource
+from jellyfin_mpv_shim.constants import APP_NAME
+
+if sys.platform == "darwin" and getattr(sys, "frozen", False):
+    # On MacOS I add a debug.log because the App bundle does not log stdout. So
+    # it's very hard to debug it.
+    # TODO: Add this only for a pyinstaller debug build
+
+    # Detect if bundled via pyinstaller.
+    # From: https://stackoverflow.com/questions/404744/
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    handler = RotatingFileHandler(
+        get_config_resource(APP_NAME, "debug.log", True),
+        maxBytes=2000,
+        backupCount=1,
+    )
+    logger.addHandler(handler)
+
 if sys.platform.startswith("win32") or sys.platform.startswith("cygwin"):
     # Detect if bundled via pyinstaller.
     # From: https://stackoverflow.com/questions/404744/
